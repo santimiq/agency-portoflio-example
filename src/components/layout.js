27f728b5fcea635dfe2a272bcf074ff4af3d88e2
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-08 18:21:03 -0500

    Adding files

---
+++ src/components/layout.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+//styled components
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { normalize } from 'styled-normalize'
+
+const GlobalStyle = createGlobalStyle`
+  ${ normalize }
+  * {
+    text-decoration: none;
+    /* cursor: none;*/
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: ${props => props.theme.background };
+    overscroll-behavior: none;
+    overflox-x: hidden;
+  }
+`
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+    const darkTheme = {
+      background: '#000',
+      text: '#fff'
+    }
+    const lightTheme = {
+      background: '#fff',
+      text: '#000'
+    }
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
