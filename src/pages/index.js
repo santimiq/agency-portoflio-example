135e54e5c16fa29d86591091ed6f18668371f848
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-18 13:07:08 -0500

    Add video and hover also title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
 import React from "react"
 import Layout from "../components/layout"
 
+//components
+import HomeBanner from "../components/homePage/HomeBanner"
+
+//context
+import { useGlobalStateContext, useGlobalDispatchContext } from '../context/GlobalContext'
+
 const IndexPage = props => {
-  return <Layout>Hey guys hope you are ready to learn!</Layout>
+  const { currentTheme, cursorStyles } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
+    dispatch({ type: 'CURSOR_TYPE', cursorType: cursorType })
+  }
+
+  return (
+    <Layout>
+      <HomeBanner />
+    </Layout>
+  )
 }
 
 export default IndexPage
